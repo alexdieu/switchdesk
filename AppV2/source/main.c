afab994fde6f0333a192e731041b33195605ad7d
Alexdieu <alexdieuveult@gmail.com>
2020-09-28 21:57:25 +0200

    Update main.c

--- AppV2/source/main.c
+++ AppV2/source/main.c
@@ -33,24 +33,16 @@ int main(int argc, char *argv[])
         {
             rc = webConfigSetFooterFixedKind(&config, WebFooterFixedKind_Hidden);
             printf("webConfigSetFooterFixedKind(): 0x%x\n", rc);
-
-            if (R_SUCCEEDED(rc))
-            {
-                rc = webConfigShow(&config, &reply);
-                printf("webConfigShow(): 0x%x\n", rc);
-                if(rc == 0x5d59){
-                    printf(CONSOLE_RED "!!!You seems to run it in applet mod , try Holding KEY_PLUS to exit.\n" CONSOLE_WHITE);
-                }
-            }
-
-            if (R_SUCCEEDED(rc))
-            {
-                rc = webReplyGetExitReason(&reply, &exitReason);
-                printf("webReplyGetExitReason(): 0x%x", rc);
-                if (R_SUCCEEDED(rc))
-                    printf(", 0x%x", exitReason);
-                printf("\n");
-                break;
+			rc = webConfigShow(&config, &reply);
+            printf("webConfigShow(): 0x%x\n", rc);
+            if(rc == 0x5d59){
+                printf(CONSOLE_RED "!!! There was an issue showing the browser window. Make sure you're running the game through an overridden title and not the album. Hold KEY_PLUS to exit.\n" CONSOLE_WHITE);
+			}
+            rc = webReplyGetExitReason(&reply, &exitReason);
+            printf("webReplyGetExitReason(): 0x%x", rc);
+            printf(", 0x%x", exitReason);
+            printf("\n");
+            break;
             }
         }
 
