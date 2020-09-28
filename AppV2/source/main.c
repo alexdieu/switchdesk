bc422636d051fa6e795f49e14c5deb814c0c30e7
Alexdieu <alexdieuveult@gmail.com>
2020-09-28 21:58:06 +0200

    Update main.c

--- AppV2/source/main.c
+++ AppV2/source/main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
         {
             rc = webConfigSetFooterFixedKind(&config, WebFooterFixedKind_Hidden);
             printf("webConfigSetFooterFixedKind(): 0x%x\n", rc);
-			rc = webConfigShow(&config, &reply);
+	    rc = webConfigShow(&config, &reply);
             printf("webConfigShow(): 0x%x\n", rc);
             if(rc == 0x5d59){
                 printf(CONSOLE_RED "!!! There was an issue showing the browser window. Make sure you're running the game through an overridden title and not the album. Hold KEY_PLUS to exit.\n" CONSOLE_WHITE);
